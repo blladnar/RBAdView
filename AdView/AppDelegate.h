7fbd537d5e6157c81ce278455f1ef55f86e46a8e
Randall <blladnar@gmail.com>
2011-11-15 17:51:52 -0500

    Initial Commit

---
+++ AdView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AdView
+//
+//  Created by Randall Brown on 11/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
